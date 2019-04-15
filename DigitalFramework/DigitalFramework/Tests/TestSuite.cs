e82646af7c54fef4c36c5739013f59a6c6b3c7e2
ReadingTestVM\Admin <pcaceres@planittesting.com>
2019-04-15 16:53:46 +0100

    Navigation

--- DigitalFramework/DigitalFramework/Tests/TestSuite.cs
+++ DigitalFramework/DigitalFramework/Tests/TestSuite.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Chrome;
 using NUnit.Framework;
 
@@ -23,6 +25,24 @@ namespace DigitalFramework.Tests
         public void OpenBrowser()
         {
             driver.Url = "https://racq.com.au";
+            driver.Manage().Window.Maximize();
+            Assert.AreEqual("Roadside Assistance, Insurance, Banking, Motoring, Travel - RACQ", driver.Title);
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            IWebElement insuranceHeader = driver.FindElement(By.XPath("//div[@class='navbar l-container']/..//li/a[text()='Insurance']"));
+
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(insuranceHeader).Build().Perform();
+
+ 
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            IWebElement getQuote = driver.FindElement(By.XPath("//div[@class='dropdown']//div/a[text()='Get insurance quote']"));
+            wait.Until(g => getQuote);
+            
+  
+            getQuote.Click();
+
         }
 
         [TearDown]
